f437248637a6d3f3b812e77688f3aa8052dfb8e3
Christopher Fry <fry.chris.c@gmail.com>
2017-08-31 13:54:57 +0100

    Added the SpaceLibrary object definitions for WebManager communication.

---
+++ UnityClient/Assets/WebManager/Runtime/Scripts/SpaceLibrary/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceLibrary
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public double CreditBalance { get; set; }
+        public int MaxCrew { get; set; }
+        public int AvailableCrew { get; set; }
+        public Ship ShipDetails { get; set; }
+    }
+}
---
+++ UnityClient/Assets/WebManager/Runtime/Scripts/SpaceLibrary/Ship.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceLibrary
+{
+    public class Ship
+    {
+        public string ShipName { get; set; }
+        public int CargoModules { get; set; }
+        public List<Cargo> CargoContents { get; set; }
+    }
+}
