5370e9cb1bcfe02bb24b581f95b72b1139bf9f60
Matt Hayward <mhayward@radiantworlds.com>
2017-09-01 16:37:51 +0100

    Conflict resolution

---
+++ UnityClient/Assets/Game/Runtime/Scripts/MarketRowButtonClickHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarketRowButtonClickHandler : MonoBehaviour, UnityEngine.EventSystems.IPointerClickHandler, UnityEngine.EventSystems.IDeselectHandler, UnityEngine.EventSystems.ISelectHandler
+{
+	public int			m_rowIndex = 0;
+
+	// TODO - looking up game objects at runtime is slow - ideally we should use the heirarchy to find a parent or something.
+
+	public void OnPointerClick( UnityEngine.EventSystems.PointerEventData eventData )
+	{
+		GameObject marketRowLayoutObject = GameObject.Find( "MarketUILayout" );
+		if( marketRowLayoutObject != null )
+		{
+			MarketUILayout marketUILayoutComponent = marketRowLayoutObject.GetComponent<MarketUILayout>();
+			marketUILayoutComponent.RowClicked( m_rowIndex );
+		}
+	}
+
+	public void OnDeselect( UnityEngine.EventSystems.BaseEventData eventData )
+	{
+		GameObject marketRowLayoutObject = GameObject.Find( "MarketUILayout" );
+		if( marketRowLayoutObject != null )
+		{
+			MarketUILayout marketUILayoutComponent = marketRowLayoutObject.GetComponent<MarketUILayout>();
+			marketUILayoutComponent.RowDeselected();
+		}
+	}
+
+	public void OnSelect( UnityEngine.EventSystems.BaseEventData eventData )
+	{
+		GameObject marketRowLayoutObject = GameObject.Find( "MarketUILayout" );
+		if( marketRowLayoutObject != null )
+		{
+			MarketUILayout marketUILayoutComponent = marketRowLayoutObject.GetComponent<MarketUILayout>();
+			marketUILayoutComponent.RowSelected( m_rowIndex );
+		}
+	}
+}
