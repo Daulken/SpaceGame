beb65a1132ffbde0614893fab0d8f59c164810ea
Christopher Fry <fry.chris.c@gmail.com>
2017-09-01 17:40:11 +0100

    Implemented a UnityClient test for SavePlayer - this increments the MaxCrew by 1.

--- UnityClient/Assets/Game/Runtime/Scripts/NetworkTest.cs
+++ UnityClient/Assets/Game/Runtime/Scripts/NetworkTest.cs
@@ -25,6 +25,7 @@ public class NetworkTest : MonoBehaviour
 	// Use this for initialization
 	void Start()
 	{
+/*
 		WebManager.LogIn("chris", "flibble",
 				(WebManager.WebResponse response) =>
 				{
@@ -36,21 +37,46 @@ public class NetworkTest : MonoBehaviour
 					}
 				}
 			);
+*/
 
 		// Request data for a player
-		WebManager.RequestResponseText(WebManager.RequestType.Post, "SpaceService.asmx/GetPlayer", new Dictionary<string, string>() { { "PlayerId", "0" } }, null,
-				(WebManager.TextWebResponse response) =>
+		WebManager.RequestResponseText(WebManager.RequestType.Post, "SpaceService.asmx/GetPlayer", new Dictionary<string, string>() { { "PlayerId", "1" } }, null,
+				(WebManager.TextWebResponse getResponse) =>
 				{
-					if (response.IsValid)
+					if (getResponse.IsValid)
 					{
-						SpaceLibrary.Player player = JsonConvert.DeserializeObject<SpaceLibrary.Player>(response.ResponseText);
+						SpaceLibrary.Player player = JsonConvert.DeserializeObject<SpaceLibrary.Player>(getResponse.ResponseText);
 						DebugHelpers.Log("Player={0}", player.ToString());
+
+						// Increase the max crew count
+						player.MaxCrew += 1;
+
+						// Re-serialise the player
+						string newPlayerData = JsonConvert.SerializeObject(player);
+
+						// Save the player data
+						WebManager.RequestResponseText(WebManager.RequestType.Post, "SpaceService.asmx/SavePlayer", new Dictionary<string, string>() { { "PlayerId", "1" }, { "PlayerData", newPlayerData } }, null,
+								(WebManager.TextWebResponse saveResponse) =>
+								{
+									if (saveResponse.IsValid)
+									{
+										DebugHelpers.Log("Player saved (Response=\"{0}\")", saveResponse.ResponseText);
+									}
+									else
+									{
+										// Log this error
+										string localisedErrorDesc = Localization.Instance.Localize("WEBERROR_" + saveResponse.ErrorCode.ToString().ToUpper());
+										DebugHelpers.LogError("SavePlayer error: {0} ({1}) - {2}", saveResponse.ErrorCode, localisedErrorDesc, saveResponse.ErrorDescription);
+									}
+								}
+							);
+
 					}
 					else
 					{
 						// Log this error
-						string localisedErrorDesc = Localization.Instance.Localize("WEBERROR_" + response.ErrorCode.ToString().ToUpper());
-						DebugHelpers.LogError("GetPlayer error: {0} ({1}) - {2}", response.ErrorCode, localisedErrorDesc, response.ErrorDescription);
+						string localisedErrorDesc = Localization.Instance.Localize("WEBERROR_" + getResponse.ErrorCode.ToString().ToUpper());
+						DebugHelpers.LogError("GetPlayer error: {0} ({1}) - {2}", getResponse.ErrorCode, localisedErrorDesc, getResponse.ErrorDescription);
 					}
 				}
 			);
