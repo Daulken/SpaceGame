c3077e27c80ef394ad6dc14373cfc5e3d7037fae
Matt Hayward <mhayward@radiantworlds.com>
2017-09-04 09:26:51 +0100

    Missing market files

---
+++ UnityClient/Assets/Game/Runtime/Scripts/MarketRow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarketRow : MonoBehaviour
+{
+	public GameObject		m_itemNameText;
+	public GameObject		m_priceText;
+	public GameObject		m_quantityText;
+	public int				m_price = 0;
+	public int				m_quantity = 0;
+
+	// Use this for initialization
+	void Start ()
+	{		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{		
+	}
+}
