819fda0412836818204c02d552bd0b0fdf763027
Christopher Fry <fry.chris.c@gmail.com>
2017-09-06 09:34:10 +0100

    Added UI.Text localisation script.

---
+++ UnityClient/Assets/Localization/Editor/Scripts/LocaliseUITextEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LocaliseUIText))]
+public class LocaliseUITextEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		// Start updating the serialized object
+		serializedObject.Update();
+
+		LocaliseUIText localizer = target as LocaliseUIText;
+
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("key"));
+
+		// Update the localisation if changed
+		bool applyLocalisation = GUILayout.Button("Apply");
+		if ((GUI.changed && Application.isPlaying) || applyLocalisation)
+		{
+			localizer.LocalizeInEditor();
+			Repaint();
+		}
+
+		// Finish updating the serialized object
+		serializedObject.ApplyModifiedProperties();
+
+		// Mark the object as dirty if any fields changed
+		if (GUI.changed)
+			EditorUtility.SetDirty(serializedObject.targetObject);
+	}
+}
---
+++ UnityClient/Assets/Localization/Runtime/Scripts/LocaliseUIText.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Simple script that lets you localize a Text object.
+/// </summary>
+[RequireComponent(typeof(Text))]
+[AddComponentMenu("UI/LocaliseUIText")]
+public class LocaliseUIText : MonoBehaviour
+{
+	[LocalizationKey]
+	public string key;
+
+	private SystemLanguage m_language;
+	private bool m_started = false;
+	private Text m_text = null;
+
+	/// <summary>
+	/// Localize the widget on enable, but only if it has been started already.
+	/// </summary>
+	void OnEnable()
+	{
+		if (m_started)
+			Localize();
+	}
+
+	/// <summary>
+	/// Localize the Text on start.
+	/// </summary>
+	void Start()
+	{
+		// Cache off the Text
+		m_text = GetComponent<Text>();
+
+		m_started = true;
+		Localize();
+	}
+
+	/// <summary>
+	/// This function is called by the Localization manager via a broadcast SendMessage.
+	/// </summary>
+	void OnLocalize(SystemLanguage newLanguage)
+	{
+		if (m_language != newLanguage)
+			Localize();
+	}
+
+	/// <summary>
+	/// Force-localize the widget, allowing this in the editor.
+	/// </summary>
+	public void LocalizeInEditor()
+	{
+		Localize(true);
+	}
+
+	/// <summary>
+	/// Force-localize the widget.
+	/// </summary>
+	public void Localize()
+	{
+		Localize(false);
+	}
+
+	private void Localize(bool inEditor)
+	{
+		m_language = Localization.Instance.GetCurrentLanguage();
+
+#if UNITY_EDITOR
+		// If in the editor, always ensure we have a valid instance
+		if (!Application.isPlaying)
+			m_text = GetComponent<Text>();
+#endif
+
+		if (m_text == null)
+			return;
+
+		// If we still don't have a key, leave the value as blank
+		string val = inEditor ? Localization.Instance.LocalizeInEditor(key) : Localization.Instance.Localize(key);
+		if (m_text.text != val)
+			m_text.text = val;
+	}
+}
