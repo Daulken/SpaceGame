9a46e877043abc8b4b9e4a69b67555bf0533e569
Christopher Fry <fry.chris.c@gmail.com>
2017-09-06 13:16:30 +0100

    Added helper SetDontDestroyOnLoad script.

---
+++ UnityClient/Assets/Game/Runtime/Scripts/SetDontDestroyOnLoad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("SPACEJAM/SetDontDestroyOnLoad")]
+public class SetDontDestroyOnLoad : MonoBehaviour
+{
+	// Use this for initialization
+	protected void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+}
