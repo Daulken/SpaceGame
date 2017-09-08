defbbd61d73f91fd3177ea9b30056e28b01ba7a6
Matt Hayward <mhayward@radiantworlds.com>
2017-09-08 10:35:01 +0100

    Conflict resolution

---
+++ UnityClient/Assets/Game/Runtime/Scripts/Gameplay/Market/InventorySlot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySlot : MonoBehaviour
+{
+	public GameObject m_itemNameText;
+	public GameObject m_quantityText;
+	public System.UInt32 m_quantity = 0;
+	public int m_materialID = 0;
+
+	// Use this for initialization
+	void Start ()
+	{		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	// Fill in the UI elements on the row - name, cost etc
+	public void PopulateUI()
+	{
+		UnityEngine.UI.Text itemNameTextComponent = m_itemNameText.GetComponent<UnityEngine.UI.Text>();
+		itemNameTextComponent.text = m_materialID.ToString();
+
+		UnityEngine.UI.Text quantityTextComponent = m_quantityText.GetComponent<UnityEngine.UI.Text>();
+		quantityTextComponent.text = m_quantity.ToString();
+	}
+}
